1e2a4e72078a0b61421063131c6bb9325d4f9d56
Vincent Levy <vicentelevy.a@gmail.com>
2021-04-20 17:03:17 -0600

    added a dependency graph  to the server

--- Server/Server/cell.cpp
+++ Server/Server/cell.cpp
@@ -1,1 +1,18 @@
 #include "cell.h"
+
+std::string Cell::get_cell_name()
+{
+	return cell_name;
+}
+
+std::string Cell::get_cell_value()
+{
+	return std::string();
+}
+
+bool Cell::is_empty()
+{
+	return &value == NULL;
+}
+
+
