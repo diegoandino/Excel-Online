cf63f0198f193ea7fbdd3f437cf7d07aa013fd54
Vincent Levy <vicentelevy.a@gmail.com>
2021-04-25 13:49:10 -0600

    added tests to the project

---
+++ Server/Tester/Tester.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Server/Formula.h"
+#include "../Server/dependency_graph.h"
+#include "../Server/cell.h"
+#include "../Server/cell_value.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tester
+{
+	TEST_CLASS(Tester)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
